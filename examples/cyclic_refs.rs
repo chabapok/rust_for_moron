aad4cc6e6254e5c0239cc63dc4cffbd18f4c0d4f
Sasha <chabapok@gmail.com>
2017-11-17 00:13:04 +0200

    - добавил...

---
+++ examples/cyclic_refs.rs
@@ -0,0 +1,15 @@
+/*
+
+ How can I implement a graph or other data structure that contains cycles?
+
+There are at least four options (discussed at length in Too Many Linked Lists):
+
+    You can implement it using Rc and Weak to allow shared ownership of nodes, although this approach pays the cost of memory management.
+    You can implement it using unsafe code using raw pointers. This will be more efficient, but bypasses Rust’s safety guarantees.
+    Using vectors and indices into those vectors. There are several available examples and explanations of this approach.
+    Using borrowed references with UnsafeCell. There are explanations and code available for this approach.
+
+    https://github.com/chabapok/r4cppp/tree/master/graphs
+    (там с примерами)
+
+*/
--- examples/Проблемы.txt.rs
+++ examples/Проблемы.txt.rs
@@ -38,4 +38,5 @@ impl StructA{
 
 
 fn main(){
+
 }
