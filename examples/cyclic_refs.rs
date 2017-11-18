5a3282ddf879f051f4e6751795b6fcc6ecfbbc1c
Sasha <chabapok@gmail.com>
2017-11-18 13:26:10 +0200

    - добавил пример циклических ссылок

--- examples/cyclic_refs.rs
+++ examples/cyclic_refs.rs
@@ -12,4 +12,8 @@ There are at least four options (discussed at length in Too Many Linked Lists):
     https://github.com/chabapok/r4cppp/tree/master/graphs
     (там с примерами)
 
+
+    пример кода
+    https://play.rust-lang.org/?gist=c1b0d133e56e5c1e4ea4ed2ecd7dd28a&version=stable
+
 */
